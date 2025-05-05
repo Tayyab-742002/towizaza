650edcf3ee0e6c24ad79713ee648242ec91bc109
talha <talhaabbasiaii@gmail.com>
2025-05-05 16:21:40 +0500

    update store page

---
+++ src/app/store/[productId]/loading.tsx
@@ -0,0 +1,5 @@
+import { Loading } from '@/components/common/Loading';
+
+export default function LoadingProductPage() {
+  return <Loading message="Loading Product..." />;
+} 
---
+++ src/app/store/loading.tsx
@@ -0,0 +1,5 @@
+import { Loading } from '@/components/common/Loading';
+
+export default function LoadingStorePage() {
+  return <Loading message="Loading Store..." />;
+} 
